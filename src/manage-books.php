efe1def607d1488bf914d6bc32374df1f8c2f51f
Janaka Dissanayake <jd@koderealm.com>
2024-10-12 09:58:52 +1100

    Validation

--- src/manage-books.php
+++ src/manage-books.php
@@ -95,15 +95,9 @@ header('location:manage-books.php');
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="form-group">
-                            <select class="form-control" id="bookStatus">
-                                <option value="">All Books</option>
-                                <option value="available">Available</option>
-                                <option value="issued">Issued</option>
-                            </select>
-                        </div>
+                        
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 text-right">
                         <a href="add-book.php" class="btn btn-primary"><i class="fa fa-plus"></i> Book</a>
                         <a href="add-author.php" class="btn btn-primary" style="margin-left: 10px;"><i class="fa fa-plus"></i> Author</a>
                         <a href="add-category.php" class="btn btn-primary" style="margin-left: 10px;"><i class="fa fa-plus"></i> Category</a>
@@ -204,24 +198,6 @@ header('location:manage-books.php');
                     </div>
                 </div>
                 <?php }} ?>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-12">
-                <nav aria-label="Page navigation">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item disabled">
-                            <a class="page-link" href="#" tabindex="-1">Previous</a>
-                        </li>
-                        <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#">Next</a>
-                        </li>
-                    </ul>
-                </nav>
             </div>
         </div>
     </div>
--- src/update-issue-bookdeails.php
+++ src/update-issue-bookdeails.php
@@ -2,30 +2,24 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['alogin'])==0)
-    {   
-header('location:index.php');
-}
-else{ 
+if(strlen($_SESSION['alogin'])==0) {   
+    header('location:index.php');
+} else { 
 
-if(isset($_POST['return']))
-{
-$rid=intval($_GET['rid']);
-$fine=$_POST['fine'];
-$rstatus=1;
-$sql="update tblissuedbookdetails set fine=:fine,RetrunStatus=:rstatus where id=:rid";
-$query = $dbh->prepare($sql);
-$query->bindParam(':rid',$rid,PDO::PARAM_STR);
-$query->bindParam(':fine',$fine,PDO::PARAM_STR);
-$query->bindParam(':rstatus',$rstatus,PDO::PARAM_STR);
-$query->execute();
+    if(isset($_POST['return'])) {
+        $rid = intval($_GET['rid']);
+        $fine = $_POST['fine'];
+        $rstatus = 1;
+        $sql = "UPDATE tblissuedbookdetails SET fine=:fine, RetrunStatus=:rstatus WHERE id=:rid";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':rid', $rid, PDO::PARAM_STR);
+        $query->bindParam(':fine', $fine, PDO::PARAM_STR);
+        $query->bindParam(':rstatus', $rstatus, PDO::PARAM_STR);
+        $query->execute();
 
-$_SESSION['msg']="Book Returned successfully";
-header('location:manage-issued-books.php');
-
-
-
-}
+        $_SESSION['msg'] = "Book Returned successfully";
+        header('location:manage-issued-books.php');
+    }
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -36,167 +30,95 @@ header('location:manage-issued-books.php');
     <meta name="author" content="" />
     <title>YBRCC Library Management System | Issued Book Details</title>
     <link rel="icon" href="assets/img/cropped-fav-32x32.png" sizes="32x32">
-    <!-- BOOTSTRAP CORE STYLE  -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME STYLE  -->
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
     <link href="assets/css/style.css" rel="stylesheet" />
-    <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-<script>
-// function for get student name
-function getstudent() {
-$("#loaderIcon").show();
-jQuery.ajax({
-url: "get_student.php",
-data:'studentid='+$("#studentid").val(),
-type: "POST",
-success:function(data){
-$("#get_student_name").html(data);
-$("#loaderIcon").hide();
-},
-error:function (){}
-});
-}
-
-//function for book details
-function getbook() {
-$("#loaderIcon").show();
-jQuery.ajax({
-url: "get_book.php",
-data:'bookid='+$("#bookid").val(),
-type: "POST",
-success:function(data){
-$("#get_book_name").html(data);
-$("#loaderIcon").hide();
-},
-error:function (){}
-});
-}
-
-</script> 
-<style type="text/css">
-  .others{
-    color:red;
-}
-
-</style>
-
-
+    <style>
+        .panel {
+            border-radius: 8px;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+        }
+        .panel-heading {
+            background-color: #007bff;
+            color: white;
+            border-top-left-radius: 8px;
+            border-top-right-radius: 8px;
+            font-weight: bold;
+        }
+        .btn {
+            border-radius: 5px;
+        }
+        .go-back {
+            margin-top: 20px;
+        }
+        .form-group label {
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
-      <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
-    <div class="content-wra
+    <?php include('includes/header.php'); ?>
     <div class="content-wrapper">
-         <div class="container">
-        <div class="row pad-botm">
-            <div class="col-md-12">
-                
-                
-                            </div>
-
-</div>
-<div class="row">
-<div class="col-md-10 col-sm-6 col-xs-12 col-md-offset-1"">
-<div class="panel panel-info">
-<div class="panel-heading">
-Issued Book Details
-</div>
-<div class="panel-body">
-<form role="form" method="post">
-<?php 
-$rid=intval($_GET['rid']);
-$sql = "SELECT tblstudents.FullName,tblbooks.BookName,tblbooks.ISBNNumber,tblissuedbookdetails.IssuesDate,tblissuedbookdetails.ReturnDate,tblissuedbookdetails.id as rid,tblissuedbookdetails.fine,tblissuedbookdetails.RetrunStatus from  tblissuedbookdetails join tblstudents on tblstudents.StudentId=tblissuedbookdetails.StudentId join tblbooks on tblbooks.id=tblissuedbookdetails.BookId where tblissuedbookdetails.id=:rid";
-$query = $dbh -> prepare($sql);
-$query->bindParam(':rid',$rid,PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{               ?>                                      
-                   
-
-
-
-<div class="form-group">
-<label>Student Name :</label>
-<?php echo htmlentities($result->FullName);?>
-</div>
-
-<div class="form-group">
-<label>Book Name :</label>
-<?php echo htmlentities($result->BookName);?>
-</div>
-
-
-<div class="form-group">
-<label>ISBN :</label>
-<?php echo htmlentities($result->ISBNNumber);?>
-</div>
-
-<div class="form-group">
-<label>Book Issued Date :</label>
-<?php echo htmlentities($result->IssuesDate);?>
-</div>
-
-
-<div class="form-group">
-<label>Book Returned Date :</label>
-<?php if($result->ReturnDate=="")
-                                            {
-                                                echo htmlentities("Not Return Yet");
-                                            } else {
-
-
-                                            echo htmlentities($result->ReturnDate);
-}
-                                            ?>
-</div>
-
-<div class="form-group">
-<label>Fine (in USD) :</label>
-<?php 
-if($result->fine=="")
-{?>
-<input class="form-control" type="text" name="fine" id="fine"  required />
-
-<?php }else {
-echo htmlentities($result->fine);
-}
-?>
-</div>
- <?php if($result->RetrunStatus==0){?>
-
-<button type="submit" name="return" id="submit" class="btn btn-info">Return Book </button>
-
- </div>
-
-<?php }}} ?>
-                                    </form>
-                            </div>
+        <div class="container">
+           
+            <div class="row">
+                <div class="col-md-10 col-sm-6 col-xs-12 col-md-offset-1">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">Issued Book Information</div>
+                        <div class="panel-body">
+                            <form role="form" method="post">
+                                <?php 
+                                $rid = intval($_GET['rid']);
+                                $sql = "SELECT tblstudents.FullName, tblbooks.BookName, tblbooks.ISBNNumber, tblissuedbookdetails.IssuesDate, tblissuedbookdetails.ReturnDate, tblissuedbookdetails.id as rid, tblissuedbookdetails.fine, tblissuedbookdetails.RetrunStatus FROM tblissuedbookdetails JOIN tblstudents ON tblstudents.StudentId=tblissuedbookdetails.StudentId JOIN tblbooks ON tblbooks.id=tblissuedbookdetails.BookId WHERE tblissuedbookdetails.id=:rid";
+                                $query = $dbh->prepare($sql);
+                                $query->bindParam(':rid', $rid, PDO::PARAM_STR);
+                                $query->execute();
+                                $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                if($query->rowCount() > 0) {
+                                    foreach($results as $result) { ?>                                      
+                                        <div class="form-group">
+                                            <label style="font-weight: bold;">Borrower Name:</label>
+                                            <p style="display: inline;padding-left: 10px;"><?php echo htmlentities($result->FullName); ?></p>
+                                        </div>
+                                        <div class="form-group">
+                                            <label style="font-weight: bold;">Book Name:</label>
+                                            <p style="display: inline;padding-left: 10px;"><?php echo htmlentities($result->BookName); ?></p>
+                                        </div>
+                                        <div class="form-group">
+                                            <label style="font-weight: bold;">ISBN:</label>
+                                            <p style="display: inline;padding-left: 10px;"><?php echo htmlentities($result->ISBNNumber); ?></p>
+                                        </div>
+                                        <div class="form-group">
+                                            <label style="font-weight: bold;">Book Issued Date:</label>
+                                            <p style="display: inline;padding-left: 10px;"><?php echo htmlentities($result->IssuesDate); ?></p>
+                                        </div>
+                                        <div class="form-group">
+                                            <label style="font-weight: bold;">Book Returned Date:</label>
+                                            <p style="display: inline;padding-left: 10px;"><?php echo ($result->ReturnDate == "") ? "Not Returned Yet" : htmlentities($result->ReturnDate); ?></p>
+                                        </div>
+                                        <div class="form-group" style="display: none;">
+                                            <label style="font-weight: bold;">Comments:</label>
+                                            <input class="form-control" type="text" name="fine" id="fine" value="<?php echo ($result->fine == "") ? '0' : htmlentities($result->fine); ?>" />
+                                        </div>
+                                        <?php if($result->RetrunStatus == 0) { ?>
+                                            <button type="submit" name="return" id="submit" class="btn btn-info">Return Book</button>
+                                            <a href="manage-issued-books.php" class="btn btn-secondary">Go Back</a>
+                                        <?php } 
+                                    }
+                                } ?>
+                            </form>
+                        
                         </div>
-                            </div>
-
+                    </div>
+                </div>
+            </div>
         </div>
-   
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
-  <?php include('includes/footer.php');?>
-      <!-- FOOTER SECTION END-->
-    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY  -->
+    <?php include('includes/footer.php'); ?>
     <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
-
 </body>
 </html>
 <?php } ?>
